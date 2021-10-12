7e5b7656965b203b0f80a829d1d755dc4c799854
Ruin0x11 <ipickering2@gmail.com>
2021-10-11 23:59:01 -0700

    Cleanup libs, add odin

---
+++ OpenNefia/Game/GameState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNefia.Game
+{
+    /// <summary>
+    /// Everything that constitutes a full save.
+    /// </summary>
+    internal class GameState
+    {
+        public InstancedMap? CurrentMap;
+
+        public GameState()
+        {
+            CurrentMap = null;
+        }
+
+        public void Save(string filepath)
+        {
+
+        }
+
+        public void Load(string filepath)
+        {
+
+        }
+    }
+}
