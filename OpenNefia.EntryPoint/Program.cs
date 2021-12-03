e290702098fc779ad26f4d01b7bfcbd63774bed6
Ruin0x11 <ipickering2@gmail.com>
2021-12-02 17:52:13 -0800

    Split project into Core, Content and EntryPoint

---
+++ OpenNefia.EntryPoint/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using OpenNefia.Core.ContentPack;
+using OpenNefia.Core.GameController;
+using OpenNefia.Core.IoC;
+using OpenNefia.Core.Log;
+using OpenNefia.Core.Reflection;
+
+namespace OpenNefia
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InitIoC();
+
+            var gc = IoCManager.Resolve<IGameController>();
+
+            if (!gc.Startup())
+            {
+                Logger.Fatal("Failed to start game controller!");
+                return;
+            }
+
+            gc.Run();
+        }
+
+        private static void InitIoC()
+        {
+            IoCManager.InitThread();
+            IoCSetup.Run();
+            IoCManager.BuildGraph();
+
+            RegisterReflection();
+        }
+
+        private static void RegisterReflection()
+        {
+            // Gets a handle to the shared and the current (client) dll.
+            IoCManager.Resolve<IReflectionManager>().LoadAssemblies(new List<Assembly>(1)
+            {
+                AppDomain.CurrentDomain.GetAssemblyByName("OpenNefia"),
+                Assembly.GetExecutingAssembly()
+            });
+        }
+    }
+}
